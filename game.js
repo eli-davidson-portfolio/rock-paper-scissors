bc0ce6571163c9c9abbcba28431f86a92248b5d6
Eli Davidson <eli@shadowlab.dev>
2022-03-02 16:39:06 -0700

    Update constructor method

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 class Game {
-    constructor (player1, player2, gameType) {
+    constructor (player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
-        this.gameType = gameType;
+        this.type = '';
         this.board = {};
     }
     checkWinCondition() {
