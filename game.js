87ca69645d10458a06d3ae6d234a45118e33374f
Eli Davidson <eli@shadowlab.dev>
2022-03-02 17:01:30 -0700

    Update reset board method

--- game.js
+++ game.js
@@ -9,7 +9,8 @@ class Game {
         //A way to check the Game’s board data for win conditions
         //A way to detect when a game is a draw (no one has won)
     }
+    
     resetBoard() {
-        //A way to reset the Game’s board to begin a new game.
+        this.board = {};
     }
 }
