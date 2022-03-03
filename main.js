d309e34bdee122ade503cd777279e0009232c95a
Eli Davidson <eli@shadowlab.dev>
2022-03-02 17:43:27 -0700

    Add render method

--- main.js
+++ main.js
@@ -4,10 +4,19 @@ var game = createGame();
 function createGame() {
     var player1 = new Player('human', 'token', 0);
     var player2 = new Player('computer', 'token', 0);
+    render(`Game Created`);
+    render(`${player1.get('name')}: ${player1.get('wins')}`);
+    render(`VS`);
+    render(`${player2.get('name')}: ${player2.get('wins')}`);
+    render(`Select Game Type:`);
     return new Game(player1, player2);
 }
 
 function setGameType(type) {
     game.type = type;
-    console.log(`game type: ${game.type}`)
+    render(`game type: ${game.type}`)
+}
+
+function render(message) {
+    console.log(message);
 }
