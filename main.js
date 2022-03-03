a8ecc67260d8f975f5d6fdc7c91c3acebbeace3f
Eli Davidson <eli@shadowlab.dev>
2022-03-02 18:54:47 -0700

    Update render and setGameType methods

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 
 var game = createGame();
-
+render()
 
 function createGame() {
     var player1 = new Player('human', 'token', 0);
     var player2 = new Player('computer', 'token', 0);
-    render(`Game Created`);
-    render(`${player1.get('name')}: ${player1.get('wins')}`);
-    render(`VS`);
-    render(`${player2.get('name')}: ${player2.get('wins')}`);
-    render(`Select Game Type: Normal`);
     return new Game(player1, player2);
 }
 
 function setGameType(type) {
     game.type = type;
-    render(`game type: ${game.type}`)
+    game.resetBoard();
+    render()
 }
 
-function render(message) {
-    console.log(message);
+function render() {
+    console.log(`____________________`);
+    console.log(`${game.player1.get('name')}: ${game.player1.get('wins')}`);
+    console.log(`VS`);
+    console.log(`${game.player2.get('name')}: ${game.player2.get('wins')}`);
+    console.log(`Selected Game Type: ${game.type}`);
+    console.log(`game board: ${game.board}`);
 }
 
-function left() {
-    moves.push(moves.shift());
-    render(moves);
-}
+// function left() {
+//     moves.push(moves.shift());
+//     render(moves);
+// }
 
-function right() {
-    moves.unshift(moves.pop());
-    render(moves);
-}
+// function right() {
+//     moves.unshift(moves.pop());
+//     render(moves);
+// }
