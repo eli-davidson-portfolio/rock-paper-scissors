0bfcfa7828cc0e1ea02836a0f6b10c9119ad5967
Eli Davidson <eli@shadowlab.dev>
2022-03-05 11:14:34 -0700

    Add data.js file to hold additional data

---
+++ data.js
@@ -0,0 +1,150 @@
+
+var data = {
+    gameType: {
+    classic: ['Rock', 'Paper', 'Scissors'],
+    advanced: ['Rock', 'Spock', 'Paper', 'Lizard', 'Scissors']
+    },
+    fighters: {
+        human: {
+            spock: {
+                name: 'Spock',
+                value: '&#128406;',
+            },
+            scissors: {
+                name: 'Scissors',
+                value: '&#9995;',
+            },
+            paper: {
+                name: 'Paper',
+                value: '&#129306;',
+            },
+            lizard: {
+                name: 'Lizard',
+                value: '&#129295;',
+            },
+            rock: {
+                name: 'Rock',
+                value: '&#128074;',
+            }
+        },
+        creature: {
+            spock: {
+                name: 'Spock',
+                value: '&#128406;'
+            },
+            scissors: {
+                name: 'Scissors',
+                value: '&#9986;'
+            },
+            paper: {
+                name: 'Paper',
+                value: '&#128196;'
+            },
+            lizard: {
+                name: 'Lizard',
+                value: '&#129422;'
+            },
+            rock: {
+                name: 'Rock',
+                value: '&#129704;'
+            }
+        }
+    },
+    avatar: {
+        human: {
+            gender: { 
+                male: {
+                young:  {
+                    name: 'Boy',
+                    value: '&#128102;'
+                },
+                adult:  {
+                    name: 'Man',
+                    value: '&#128104;'
+                },
+                old:    {
+                    name: 'Old Man',
+                    value: '&#128116;'
+                }
+                },
+                nb: {
+                    young: {
+                        name: 'Child',
+                        value: '&#129490;'
+                    },
+                    adult: {
+                        name: 'Person',
+                        value: '&#129489;'
+                    },
+                    old:  {
+                        name: 'Older person',
+                        value: '&#129491;'
+                    }
+                },
+                female: {
+                    young: {
+                        name: 'Girl',
+                        value: '&#128103;'
+                    },
+                    adult: {
+                        name: 'Woman',
+                        value: '&#128105;'
+                    },
+                    old: {
+                        name: 'Old Woman',
+                        value: '&#128117;'
+                    },
+                }
+            },
+            skinTone: [
+                {
+                    name: 'Light',
+                    value: '&#127995;'
+                },
+                {
+                    name: 'Medium-light',
+                    value: '&#127996;'
+                },
+                {
+                    name: 'Medium',
+                    value: '&#127997;'
+                },
+                {
+                    name: 'Medium-dark',
+                    value: '&#127998;'
+                },
+                {
+                    name: 'Dark',
+                    value: '&#127999;'
+                },
+            ]
+        },
+        creature: [
+            {
+                name: 'Robot',
+                value: '&#129302;'
+            },
+            {
+                name: 'Fox',
+                value: '&#129418;'
+            },
+            {
+                name: 'Ghost',
+                value: '&#128123;'
+            },
+            {
+                name: 'Alien',
+                value: '&#128125;'
+            },
+            {
+                name: 'Panda',
+                value: '&#128060;'
+            },
+            {
+                name: 'Unicorn',
+                value: '&#129412;'
+            },
+        ]
+    }
+}
+
