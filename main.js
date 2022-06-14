c538afc8c15e141a1356a1ff43ac5c4c55ed6121
Eli Davidson <eli@shadowlab.dev>
2022-06-13 22:27:10 -0600

    Deploy to Heroku

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const game = new Game();
+let apiCall = () => fetch('https://emojihub.herokuapp.com/api/all').then(response => {
+   (response.ok) ? response.json() : console.log("response not okay")
+}).then(data => console.log("data is", data))
+
+
+let game;
+
+apiCall().then(game = new Game())
+
 
 const render = () => document.querySelector('.main').innerHTML = game.show()
 
 const displayResults = () => game.result.forEach((result, index) => setTimeout(() => document.querySelector('.prompt').innerHTML = result, index * 3000)) 
 
 game.menu.show('menu');
+
